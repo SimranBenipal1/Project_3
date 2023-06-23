66716dea71d0ff6d2e779f55ade7eedda20f37b9
Simran <SBenipal@skillstorm.com>
2023-06-23 10:22:53 -0400

    added testing functions to helloworldcontroller

--- vanguard-capstone-project/src/main/java/com/skillstorm/demo/controllers/HelloWorldController.java
+++ vanguard-capstone-project/src/main/java/com/skillstorm/demo/controllers/HelloWorldController.java
@@ -1,12 +1,17 @@
 package com.skillstorm.demo.controllers;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
-
+	
 	@Value("${my.env.mode}") 
 	private String mode;
 	
@@ -14,4 +19,12 @@ public class HelloWorldController {
 	public String helloWorld() {
 		return "You are Currently in: " + mode; 
 	}
+	
+	@GetMapping("/userinfo")
+	@ResponseBody // Send the data as JSON
+	public Map<String, Object> userInfo(@AuthenticationPrincipal OAuth2User user) {
+		// Info about the user
+		return user.getAttributes();
+	}
+	
 }
