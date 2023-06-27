950be9aa08bf11b342858d605149175846e543b3
Simran <SBenipal@skillstorm.com>
2023-06-27 14:33:55 -0400

    Added Access Token Request

--- vanguard-capstone-project/src/main/java/com/skillstorm/demo/controllers/HelloWorldController.java
+++ vanguard-capstone-project/src/main/java/com/skillstorm/demo/controllers/HelloWorldController.java
@@ -2,8 +2,13 @@ package com.skillstorm.demo.controllers;
 
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +22,9 @@ public class HelloWorldController {
 	
 	@Value("${my.env.mode}") 
 	private String mode;
+	
+	@Autowired
+	private OAuth2AuthorizedClientService clientService;
 	
 	@GetMapping("/hello")
 	public String helloWorld() {
@@ -37,6 +45,19 @@ public class HelloWorldController {
 	public Map<String, Object> userInfo(@AuthenticationPrincipal OAuth2User user) {
 		// Info about the user
 		return user.getAttributes();
+	}
+	
+	// Return access token
+	@GetMapping("/access")
+	@ResponseBody
+	public String accessToken(Authentication auth) {
+		if (auth instanceof OAuth2AuthenticationToken) {
+			OAuth2AuthenticationToken authToken = (OAuth2AuthenticationToken) auth;
+			OAuth2AuthorizedClient client = clientService
+					.loadAuthorizedClient(authToken.getAuthorizedClientRegistrationId(), authToken.getName());
+			return client.getAccessToken().getTokenValue();
+		}
+		return "";
 	}
 	
 }
